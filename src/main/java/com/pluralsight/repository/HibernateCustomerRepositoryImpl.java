5c0ebe3d055f2957da4508500549ac23f932d92e
Josh Hedstrom <josh.hedstrom@gmail.com>
2018-09-25 08:06:15 -0600

    scaffolded basic runnable application

--- src/main/java/com/pluralsight/repository/HibernateCustomerRepositoryImpl.java
+++ src/main/java/com/pluralsight/repository/HibernateCustomerRepositoryImpl.java
@@ -1,5 +1,26 @@
 package com.pluralsight.repository;
 
-public class HibernateCustomerRepositoryImpl {
+import java.util.ArrayList;
+import java.util.List;
+
+import com.pluralsight.model.Customer;
+
+public class HibernateCustomerRepositoryImpl implements CustomerRepository {
+	
+	/* (non-Javadoc)
+	 * @see com.pluralsight.repository.CustomerRepository#findAll()
+	 */
+	public List<Customer> findAll() {
+		List<Customer> customers = new ArrayList<Customer>();
+		
+		Customer customer = new Customer();
+		
+		customer.setFirstname("Josh");
+		customer.setLastname("Hedstrom");
+		
+		customers.add(customer);
+		
+		return customers;
+	}
 
 }
