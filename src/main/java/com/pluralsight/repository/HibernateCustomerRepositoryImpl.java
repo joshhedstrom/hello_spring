2c93fedb785b6b59339c9d3b797a7ccc6cf8d427
Josh Hedstrom <josh.hedstrom@gmail.com>
2018-09-25 11:08:51 -0600

    added db user property

--- src/main/java/com/pluralsight/repository/HibernateCustomerRepositoryImpl.java
+++ src/main/java/com/pluralsight/repository/HibernateCustomerRepositoryImpl.java
@@ -3,23 +3,29 @@ package com.pluralsight.repository;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Repository;
+
 import com.pluralsight.model.Customer;
 
+@Repository("customerRepository")
 public class HibernateCustomerRepositoryImpl implements CustomerRepository {
-	
-	/* (non-Javadoc)
-	 * @see com.pluralsight.repository.CustomerRepository#findAll()
-	 */
+
+	@Value("${dbUsername}")
+//	private String dbUsername;
+
+	@Override
 	public List<Customer> findAll() {
+//		System.out.println(dbUsername);
 		List<Customer> customers = new ArrayList<Customer>();
-		
+
 		Customer customer = new Customer();
-		
+
 		customer.setFirstname("Josh");
 		customer.setLastname("Hedstrom");
-		
+
 		customers.add(customer);
-		
+
 		return customers;
 	}
 
