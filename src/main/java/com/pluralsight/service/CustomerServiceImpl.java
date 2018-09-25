5081ea80008b2ffc31d0c51efbae5094a868b771
Josh Hedstrom <josh.hedstrom@gmail.com>
2018-09-25 10:45:39 -0600

    changed to prototype scope

--- src/main/java/com/pluralsight/service/CustomerServiceImpl.java
+++ src/main/java/com/pluralsight/service/CustomerServiceImpl.java
@@ -2,6 +2,7 @@ package com.pluralsight.service;
 
 import java.util.List;
 
+import org.springframework.beans.factory.config.ConfigurableBeanFactory;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Service;
 
@@ -9,7 +10,7 @@ import com.pluralsight.model.Customer;
 import com.pluralsight.repository.CustomerRepository;
 
 @Service("customerService")
-@Scope("singleton")
+@Scope(ConfigurableBeanFactory.SCOPE_PROTOTYPE)
 
 public class CustomerServiceImpl implements CustomerService {
 
